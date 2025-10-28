2eb0cc7c2776166e358925f0111d912d0d190191
Gopal Ravindhran <gopalr@jpl.nasa.gov>
2025-10-28 18:59:47 +0000

    Formatting

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,32 +1,56 @@
+-- =========================================================
+-- Leader key
+-- =========================================================
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- vim.api.nvim_set_keymap('n', '<leader>f', ":lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>b', ":lua require('fzf-lua').buffers()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>g', ":lua require('fzf-lua').grep()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>l', ":lua require('fzf-lua').lines()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>hd', ":lua require('gitsigns').diffthis()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>hs', ":lua require('gitsigns').stage_hunk()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>hr', ":lua require('gitsigns').reset_hunk()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>nt", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>sb", ":Telescope buffers<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>cp", ":let @+ = expand('%:p')<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", ">", ">gv")
+
+-- =========================================================
+-- Helper for normal mode mappings
+-- =========================================================
+local nmap = function(lhs, rhs, opts)
+  vim.keymap.set("n", lhs, rhs, opts or { silent = true })
+end
+
+-- =========================================================
+-- Normal mode mappings
+-- =========================================================
+
+-- File navigation
+nmap("<leader>pv", vim.cmd.Ex, { desc = "Open explorer" })
+nmap("<leader>nt", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+nmap("<C-s>", ":Oil<CR>", { desc = "Open Oil file browser" })
+
+-- Buffers
+nmap("<leader>sb", ":Telescope buffers<CR>", { desc = "List buffers" })
+
+-- Switch to alternate file
+nmap("<Leader>w", "<C-6>", { desc = "Switch to alternate file" })
+
+-- Copy entire buffer
+nmap("<Leader>ya", "ggVGy", { desc = "Yank entire buffer" })
+
+-- Copy current file path
+nmap("<leader>cp", ":let @+ = expand('%:p')<CR>", { desc = "Copy current file path" })
+
+-- Yank GitHub permalink (normal and visual modes)
+vim.keymap.set({ "n", "x" }, "<leader>gy", function()
+  require("gh-permalink").yank()
+end, { desc = "Yank GitHub permalink to clipboard" })
+
+-- =========================================================
+-- Visual mode mappings
+-- =========================================================
+vim.keymap.set("v", ">", ">gv") -- Keep selection when indenting
 vim.keymap.set("v", "<", "<gv")
 
-function copy()
-	if vim.v.event.operator == "y" then
-		local yanked_text = vim.fn.getreg('"') -- get the yanked text from unnamed register
-		require("osc52").copy(yanked_text) -- send it via OSC52d
-	end
+-- =========================================================
+-- OSC52 clipboard integration on yank
+-- =========================================================
+local function copy()
+  if vim.v.event.operator == "y" then
+    local yanked_text = vim.fn.getreg('"') -- Get yanked text
+    require("osc52").copy(yanked_text)     -- Send via OSC52
+  end
 end
+
 vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
 
-vim.keymap.set("n", "<Leader>w", "<C-6>")
-vim.keymap.set("n", "<Leader>ya", "ggVGy")
-
-vim.keymap.set("n", "<C-s>", ":Oil<CR>")
-
--- Yank permalink (normal: line, visual: range)
-vim.keymap.set({ "n", "x" }, "<leader>gy", function()
-	require("gh-permalink").yank()
-end, { desc = "Yank GitHub permalink to clipboard" })
