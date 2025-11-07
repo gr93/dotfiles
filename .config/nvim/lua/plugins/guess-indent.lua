7bb95163f7e9ee011a7d43a17f90f5dcc7e659bc
gr93 <gopalr@GOPAL-ATLANTA32.localdomain>
2025-11-06 23:17:56 -0800

    Add guess-indent

--- .config/nvim/lua/plugins/guess-indent.lua
+++ .config/nvim/lua/plugins/guess-indent.lua
@@ -1,1 +1,22 @@
 return {
+  {
+    "nmac427/guess-indent.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("guess-indent").setup {
+        auto_cmd = true,
+        override_editorconfig = false,
+        filetype_exclude = { "netrw", "tutor" },
+        buftype_exclude = { "help", "nofile", "terminal", "prompt" },
+        on_tab_options = { ["expandtab"] = false },
+        on_space_options = {
+          ["expandtab"] = true,
+          ["tabstop"] = "detected",
+          ["softtabstop"] = "detected",
+          ["shiftwidth"] = "detected",
+        },
+      }
+    end,
+  },
+}
+
